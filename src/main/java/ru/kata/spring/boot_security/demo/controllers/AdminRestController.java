cb29019cf654a3e4e28b0154f9ebb27e96b1a047
HautZ532 <113337899+HautZ532@users.noreply.github.com>
2023-06-23 13:51:55 +0300

    Update

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminRestController.java
@@ -10,7 +10,7 @@ import ru.kata.spring.boot_security.demo.services.UserService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/admin")
 public class AdminRestController {
     private final UserService userService;
 
@@ -19,31 +19,36 @@ public class AdminRestController {
         this.userService = userService;
     }
 
-    @GetMapping("/admin")
+    @GetMapping()
     public ResponseEntity<List<User>> showAllUsers() {
-        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+//        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+        return ResponseEntity.ok().body(userService.getAllUsers());
     }
 
-    @GetMapping("/admin/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<User> getUser(@PathVariable Long id) {
-        return new ResponseEntity<>(userService.getUser(id), HttpStatus.OK);
+//        return new ResponseEntity<>(userService.getUser(id), HttpStatus.OK);
+        return ResponseEntity.ok().body(userService.getUser(id));
     }
 
-    @PatchMapping("/admin")
+    @PatchMapping()
     public ResponseEntity<HttpStatus> editUser(@RequestBody User user) {
         userService.editUser(user);
-        return new ResponseEntity<>(HttpStatus.OK);
+//        return new ResponseEntity<>(HttpStatus.OK);
+        return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/admin")
+    @PostMapping()
     public ResponseEntity<HttpStatus> addNewUser(@RequestBody User user) {
         userService.saveUser(user);
-        return new ResponseEntity<>(HttpStatus.OK);
+//        return new ResponseEntity<>(HttpStatus.OK);
+        return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/admin/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<HttpStatus> deleteUser(@PathVariable Long id) {
         userService.removeUser(id);
-        return new ResponseEntity<>(HttpStatus.OK);
+//        return new ResponseEntity<>(HttpStatus.OK);
+        return ResponseEntity.ok().build();
     }
 }
